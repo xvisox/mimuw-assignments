b84e847ef4645a394f57ecae0a9a93a5dafffd77
xvisox <hubertmirek@gmail.com>
2022-08-02 17:59:09 +0200

    drzewa przedziałowe

---
+++ OKI/SegmentTree/Browary.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define N 10000
+using namespace std;
+
+int n, koszt[N], odl[N], result;
+int kosztL, kosztP, ileL, ileP, odlL, odlP;
+
+int main() {
+    cin >> n;
+    int i;
+    for (i = 0; i < n; i++) {
+        scanf("%d %d", &koszt[i], &odl[i]);
+    }
+    for (i = 1; i < n; i++) {
+        // rezygnacja
+    }
+
+}
