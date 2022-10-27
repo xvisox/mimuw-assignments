6a88afcbdc3f57075c713fc96bfc2ceac4208501
Marcin <92443163+kapustaikwas27@users.noreply.github.com>
2022-10-27 09:56:22 +0200

    Pierwsze kroki
    Usunąłem: using namespace std, struct Sequence

--- hash.cc
+++ hash.cc
@@ -6,48 +6,38 @@
 #include <unordered_map>
 #include <unordered_set>
 
-using namespace std;
-
-using hash_function_t = uint64_t (*)(uint64_t const *, size_t);
-using hash_functions_t = unordered_map<unsigned long, hash_function_t>;
-using Sequence = struct Sequence;
 using Hasher = struct Hasher;
-using hashtbl_t = unordered_set<Sequence, Hasher>;
-using hashtbls_t = unordered_map<unsigned long, hashtbl_t>;
+using sequence_t = std::pair<std::vector<uint64_t>, unsigned long>;
+using hash_function_t = uint64_t (*)(uint64_t const *, size_t);
+using hash_functions_t = std::unordered_map<unsigned long, hash_function_t>;
+using hash_tbl_t = std::unordered_set<sequence_t, Hasher>;
+using hash_tbls_t = std::unordered_map<unsigned long, hash_tbl_t>;
 
 unsigned long nextId = 0;
 hash_functions_t hashFunctions;
-hashtbls_t hashTables;
-
-struct Sequence {
-    vector<uint64_t> vec;
-    unsigned long id;
-
-    bool operator==(const Sequence& other) const {
-        return vec == other.vec;
-    }
-};
+hash_tbls_t hashTables;
 
 struct Hasher {
-    size_t operator()(const Sequence& seq) const {
-        hash_functions_t::iterator it = hashFunctions.find(seq.id);
-        return it->second(&seq.vec[0], seq.vec.size());
+    size_t operator()(const sequence_t& sequence) const {
+        hash_functions_t::iterator it = hashFunctions.find(sequence.second);
+        return it->second(&sequence.first[0], sequence.first.size());
     }
 };
 
 unsigned long hash_create(hash_function_t hash_function) {
-    hashtbl_t hashtbl;
+    hash_tbl_t hash_tbl;
     hashFunctions[nextId] = hash_function;
-    hashTables[nextId] = hashtbl;
+    hashTables[nextId] = hash_tbl;
     return nextId++;
 }
 
 void hash_delete(unsigned long id) {
+    hashFunctions.erase(id);
     hashTables.erase(id);
 }
 
 size_t hash_size(unsigned long id) {
-    hashtbls_t::iterator it = hashTables.find(id);
+    hash_tbls_t::iterator it = hashTables.find(id);
 
     if (it == hashTables.end())
         return 0;
@@ -59,18 +49,18 @@ bool hash_insert(unsigned long id, uint64_t const * seq, size_t size) {
     if (seq == NULL || size == 0)
         return false;
 
-    hashtbls_t::iterator it = hashTables.find(id);
+    hash_tbls_t::iterator it = hashTables.find(id);
 
     if (it == hashTables.end())
         return false;
 
-    vector<uint64_t> vec(seq, seq + size);
-    Sequence s = {vec, id};
+    std::vector<uint64_t> vec(seq, seq + size);
+    sequence_t sequence = make_pair(vec, id);
 
-    if (it->second.find(s) != it->second.end())
+    if (it->second.find(sequence) != it->second.end())
         return false;
 
-    it->second.insert(s);
+    it->second.insert(sequence);
 
     return true;
 }
@@ -79,24 +69,24 @@ bool hash_remove(unsigned long id, uint64_t const * seq, size_t size) {
     if (seq == NULL || size == 0)
         return false;
 
-    hashtbls_t::iterator it = hashTables.find(id);
+    hash_tbls_t::iterator it = hashTables.find(id);
 
     if (it == hashTables.end())
         return false;
 
-    vector<uint64_t> vec(seq, seq + size);
-    Sequence s = {vec, id};
+    std::vector<uint64_t> vec(seq, seq + size);
+    sequence_t sequence = make_pair(vec, id);
 
-    if (it->second.find(s) == it->second.end())
+    if (it->second.find(sequence) == it->second.end())
         return false;
 
-    it->second.erase(s);
+    it->second.erase(sequence);
 
     return true;
 }
 
 void hash_clear(unsigned long id) {
-    hashtbls_t::iterator it = hashTables.find(id);
+    hash_tbls_t::iterator it = hashTables.find(id);
 
     if (it != hashTables.end())
         return it->second.clear();
@@ -106,15 +96,15 @@ bool hash_test(unsigned long id, uint64_t const * seq, size_t size) {
     if (seq == NULL || size == 0)
         return false;
 
-    hashtbls_t::iterator it = hashTables.find(id);
+    hash_tbls_t::iterator it = hashTables.find(id);
 
     if (it == hashTables.end())
         return false;
 
-    vector<uint64_t> vec(seq, seq + size);
-    Sequence s = {vec, id};
+    std::vector<uint64_t> vec(seq, seq + size);
+    sequence_t sequence = make_pair(vec, id);
 
-    if (it->second.find(s) == it->second.end())
+    if (it->second.find(sequence) == it->second.end())
         return false;
     else
         return true;
