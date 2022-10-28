eac2411689d023fe45fd50dd72e1a5a2489e594b
Marcin <92443163+kapustaikwas27@users.noreply.github.com>
2022-10-28 21:19:52 +0200

    Wersja finalna?
    Ujednolicenie stylu na _. JestemOgólnieFanemTakiegoStylu, ale w tym zadaniu przez interfejs jest narzucony styl _.

--- hash.cc
+++ hash.cc
@@ -14,103 +14,113 @@ bool const debug = true;
 #include <unordered_set>
 #include "hash.h"
 
-#define debugStream if (!debug) {} else cerrStream()
+#define debug_stream \
+    if (debug) \
+        cerr_stream()
 
 namespace {
     using Hasher = struct Hasher;
     using sequence_t = std::pair<std::vector<uint64_t>, unsigned long>;
     using hash_function_t = uint64_t (*)(uint64_t const *, size_t);
     using hash_functions_t = std::unordered_map<unsigned long, hash_function_t>;
-    using hash_table_t = std::unordered_set<sequence_t, Hasher>;
-    using hash_tables_t = std::unordered_map<unsigned long, hash_table_t>;
+    using hash_tbl_t = std::unordered_set<sequence_t, Hasher>;
+    using hash_tbls_t = std::unordered_map<unsigned long, hash_tbl_t>;
 
-    unsigned long nextId = 0;
+    unsigned long next_id = 0;
 
-    std::ostream &cerrStream() {
-        static std::ios_base::Init mInitializer;
+    std::ostream &cerr_stream() {
+        static std::ios_base::Init initializer;
         return std::cerr;
     }
 
-    hash_functions_t &getHashFunctions() {
-        static hash_functions_t hashFunctions;
-        return hashFunctions;
+    hash_functions_t &get_hash_functions() {
+        static hash_functions_t hash_functions;
+        return hash_functions;
     }
 
     struct Hasher {
         size_t operator()(const sequence_t &sequence) const {
-            auto hashFunctionIt = getHashFunctions().find(sequence.second);
-            return hashFunctionIt->second(&sequence.first[0], sequence.first.size());
+            auto it = get_hash_functions().find(sequence.second);
+            return it->second(&sequence.first[0], sequence.first.size());
         }
     };
 
-    hash_tables_t &getHashTables() {
-        static hash_tables_t hashTables;
-        return hashTables;
+    hash_tbls_t &get_hash_tables() {
+        static hash_tbls_t hash_tbls;
+        return hash_tbls;
     }
 
-    std::string seqToString(uint64_t const *seq, size_t size) {
+    std::string seq_to_string(uint64_t const *seq, size_t size) {
         if (seq == nullptr) {
-            std::string s = "NULL";
-            return s;
+            return "NULL";
         }
 
         std::stringstream ss;
+
         ss << '\"';
+
         if (size > 0) {
             for (size_t i = 0; i < size - 1; i++)
                 ss << seq[i] << ' ';
 
             ss << seq[size - 1];
         }
+
         ss << '\"';
 
         return ss.str();
     }
 
-    void logHashCreate(const char *functionName, hash_function_t hash_function) {
-        debugStream << functionName << '(' << &hash_function << ')' << std::endl;
+    void print_input_1(const char *function_name,
+                       hash_function_t hash_function) {
+        debug_stream << function_name << '(' << &hash_function << ')'
+                     << std::endl;
     }
 
-    void logFunctionCall(const char *functionName, unsigned long id) {
-        debugStream << functionName << '(' << id << ')' << std::endl;
+    void print_input_2(const char *function_name, unsigned long id) {
+        debug_stream << function_name << '(' << id << ')' << std::endl;
     }
 
-    void logInputSequence(const char *functionName, unsigned long id,
-                          uint64_t const *seq, size_t size) {
-        debugStream << functionName << '(' << id << ", "
-                    << seqToString(seq, size) << ", " << size << ')'
-                    << std::endl;
+    void print_input_3(const char *function_name, unsigned long id,
+                       uint64_t const *seq, size_t size) {
+        debug_stream << function_name << '(' << id << ", "
+                     << seq_to_string(seq, size) << ", " << size << ')'
+                     << std::endl;
     }
 
-    void logHashInfo(const char *functionName, unsigned long id,
-                     const char *info) {
-        debugStream << functionName << ": hash table #" << id << ' ' << info
-                    << std::endl;
+    void print_operation_1(const char *function_name, unsigned long id,
+                           const char *info) {
+        debug_stream << function_name << ": hash table #" << id << ' ' << info
+                     << std::endl;
     }
 
-    void logHashSize(const char *functionName, unsigned long id,
-                     size_t size, const char *info_2) {
-        debugStream << functionName << ": hash table #" << id << ' ' << "contains"
-                    << ' ' << size << ' ' << info_2 << std::endl;
+    void print_operation_2(const char *function_name, unsigned long id,
+                           const char *info_1, size_t size,
+                           const char *info_2) {
+        debug_stream << function_name << ": hash table #" << id << ' '
+                     << info_1 << ' ' << size << ' ' << info_2 << std::endl;
     }
 
-    void logSequenceInfo(const char *functionName, unsigned long id,
-                         uint64_t const *seq, size_t size,
+    void print_operation_3(const char *function_name, unsigned long id,
+                         const char *info_1, uint64_t const *seq, size_t size,
                          const char *info_2) {
-        debugStream << functionName << ": hash table #" << id << ", sequence"
-                    << ' ' << seqToString(seq, size) << ' ' << info_2 << std::endl;
+        debug_stream << function_name << ": hash table #" << id << info_1
+                     << ' ' << seq_to_string(seq, size) << ' ' << info_2
+                     << std::endl;
     }
 
-    bool checkInput(const char *functionName, uint64_t const *seq, size_t size) {
+    bool check_input(const char *function_name, uint64_t const *seq,
+                     size_t size) {
         bool correct = true;
 
         if (seq == nullptr) {
-            debugStream << functionName << ": invalid pointer (NULL)" << std::endl;
+            debug_stream << function_name << ": invalid pointer (NULL)"
+                         << std::endl;
             correct = false;
         }
 
         if (size == 0) {
-            debugStream << functionName << ": invalid size (0)" << std::endl;
+            debug_stream << function_name << ": invalid size (0)" << std::endl;
             correct = false;
         }
 
@@ -121,128 +131,141 @@ namespace {
 namespace jnp1 {
     unsigned long hash_create(hash_function_t hash_function) {
         assert(hash_function != nullptr);
-        logHashCreate(__func__, hash_function);
+        print_input_1(__func__, hash_function);
 
-        hash_table_t hash_table;
-        getHashFunctions()[nextId] = hash_function;
-        getHashTables()[nextId] = hash_table;
+        hash_tbl_t hash_tbl;
+        get_hash_functions()[next_id] = hash_function;
+        get_hash_tables()[next_id] = hash_tbl;
+        print_operation_1(__func__, next_id, "created");
 
-        logHashInfo(__func__, nextId, "created");
-        return nextId++;
+        return next_id++;
     }
 
     void hash_delete(unsigned long id) {
-        logFunctionCall(__func__, id);
+        print_input_2(__func__, id);
+        auto it = get_hash_tables().find(id);
 
-        auto hashTableIt = getHashTables().find(id);
-        if (hashTableIt == getHashTables().end()) {
-            logHashInfo(__func__, id, "does not exist");
+        if (it == get_hash_tables().end()) {
+            print_operation_1(__func__, id, "does not exist");
         } else {
-            getHashFunctions().erase(id);
-            getHashTables().erase(hashTableIt);
-            logHashInfo(__func__, id, "deleted");
+            get_hash_functions().erase(id);
+            get_hash_tables().erase(it);
+            print_operation_1(__func__, id, "deleted");
         }
     }
 
     size_t hash_size(unsigned long id) {
-        logFunctionCall(__func__, id);
+        print_input_2(__func__, id);
+        auto it = get_hash_tables().find(id);
 
-        auto hashTableIt = getHashTables().find(id);
-        if (hashTableIt == getHashTables().end()) {
-            logHashInfo(__func__, id, "does not exist");
+        if (it == get_hash_tables().end()) {
+            print_operation_1(__func__, id, "does not exist");
             return 0;
         } else {
-            logHashSize(__func__, id, hashTableIt->second.size(), "element(s)");
-            return hashTableIt->second.size();
+            print_operation_2(__func__, id, "contains", it->second.size(),
+                              "element(s)");
+            return it->second.size();
         }
     }
 
     bool hash_insert(unsigned long id, uint64_t const *seq, size_t size) {
-        logInputSequence(__func__, id, seq, size);
-        bool correct = checkInput(__func__, seq, size);
-        if (!correct) return false;
+        print_input_3(__func__, id, seq, size);
+        bool correct = check_input(__func__, seq, size);
 
-        auto hashTableIt = getHashTables().find(id);
-        if (hashTableIt == getHashTables().end()) {
-            logHashInfo(__func__, id, "does not exist");
+        if (!correct)
+            return false;
+
+        auto it = get_hash_tables().find(id);
+
+        if (it == get_hash_tables().end()) {
+            print_operation_1(__func__, id, "does not exist");
             return false;
         }
 
         std::vector<uint64_t> vec(seq, seq + size);
         sequence_t sequence = make_pair(vec, id);
 
-        if (hashTableIt->second.find(sequence) != hashTableIt->second.end()) {
-            logSequenceInfo(__func__, id, seq, size, "was present");
+        if (it->second.find(sequence) != it->second.end()) {
+            print_operation_3(__func__, id, ", sequence", seq, size,
+                              "was present");
             return false;
         }
 
-        hashTableIt->second.insert(sequence);
-        logSequenceInfo(__func__, id, seq, size, "inserted");
+        it->second.insert(sequence);
+        print_operation_3(__func__, id, ", sequence", seq, size, "inserted");
 
         return true;
     }
 
     bool hash_remove(unsigned long id, uint64_t const *seq, size_t size) {
-        logInputSequence(__func__, id, seq, size);
-        bool correct = checkInput(__func__, seq, size);
-        if (!correct) return false;
+        print_input_3(__func__, id, seq, size);
+        bool correct = check_input(__func__, seq, size);
 
-        auto hashTableIt = getHashTables().find(id);
-        if (hashTableIt == getHashTables().end()) {
-            logHashInfo(__func__, id, "does not exist");
+        if (!correct)
+            return false;
+
+        auto it = get_hash_tables().find(id);
+
+        if (it == get_hash_tables().end()) {
+            print_operation_1(__func__, id, "does not exist");
             return false;
         }
 
         std::vector<uint64_t> vec(seq, seq + size);
         sequence_t sequence = make_pair(vec, id);
 
-        if (hashTableIt->second.find(sequence) == hashTableIt->second.end()) {
-            logSequenceInfo(__func__, id, seq, size, "was not present");
+        if (it->second.find(sequence) == it->second.end()) {
+            print_operation_3(__func__, id, ", sequence", seq, size,
+                              "was not present");
             return false;
         }
 
-        hashTableIt->second.erase(sequence);
-        logSequenceInfo(__func__, id, seq, size, "removed");
+        it->second.erase(sequence);
+        print_operation_3(__func__, id, ", sequence", seq, size, "removed");
 
         return true;
     }
 
     void hash_clear(unsigned long id) {
-        logFunctionCall(__func__, id);
+        print_input_2(__func__, id);
+        auto it = get_hash_tables().find(id);
 
-        auto hashTableIt = getHashTables().find(id);
-
-        if (hashTableIt == getHashTables().end()) {
-            logHashInfo(__func__, id, "does not exist");
+        if (it == get_hash_tables().end()) {
+            print_operation_1(__func__, id, "does not exist");
         } else {
-            if (hashTableIt->second.empty()) {
-                logHashInfo(__func__, id, "was empty");
+            if (it->second.empty()) {
+                print_operation_1(__func__, id, "was empty");
             } else {
-                logHashInfo(__func__, id, "cleared");
-                hashTableIt->second.clear();
+                print_operation_1(__func__, id, "cleared");
+                it->second.clear();
             }
         }
     }
 
     bool hash_test(unsigned long id, uint64_t const *seq, size_t size) {
-        logInputSequence(__func__, id, seq, size);
-        bool correct = checkInput(__func__, seq, size);
-        if (!correct) return false;
+        print_input_3(__func__, id, seq, size);
+        bool correct = check_input(__func__, seq, size);
 
-        auto hashTableIt = getHashTables().find(id);
-        if (hashTableIt == getHashTables().end()) {
-            logHashInfo(__func__, id, "does not exist");
+        if (!correct)
+            return false;
+
+        auto it = get_hash_tables().find(id);
+
+        if (it == get_hash_tables().end()) {
+            print_operation_1(__func__, id, "does not exist");
             return false;
         }
 
         std::vector<uint64_t> vec(seq, seq + size);
         sequence_t sequence = make_pair(vec, id);
 
-        if (hashTableIt->second.find(sequence) == hashTableIt->second.end()) {
-            logSequenceInfo(__func__, id, seq, size, "is not present");
+        if (it->second.find(sequence) == it->second.end()) {
+            print_operation_3(__func__, id, ", sequence", seq, size,
+                              "is not present");
             return false;
         } else {
-            logSequenceInfo(__func__, id, seq, size, "is present");
+            print_operation_3(__func__, id, ", sequence", seq, size,
+                              "is present");
             return true;
         }
     }
