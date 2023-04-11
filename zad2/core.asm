ed04d147e16118f754048e4743ea9b800020248f
xvisox <hubertmirek@gmail.com>
2023-04-11 12:12:19 +0200

    Probably finished second assignment.

--- zad2/core.asm
+++ zad2/core.asm
@@ -1,164 +1,179 @@
+; Hubert Michalski hm438596
 global core
-
-%macro call_func_with_stack_alignment 1
-    mov rdi, r12
-    mov r15, rsp
-    and r15, 0b1111
-    sub rsp, r15
-    call %1
-    add rsp, r15
-%endmacro
 
 extern get_value
 extern put_value
 
+%macro CALL_WITH_STACK_ALIGN 1          ; calls a function with aligned stack
+                                        ; [%1] - function to call
+    mov rdi, r12                        ; pass core identifier
+    mov r15, rsp                        ; get the current stack pointer
+    and r15, 0b1111                     ; get the last 4 bits
+    sub rsp, r15                        ; align the stack
+    call %1
+    add rsp, r15                        ; restore the stack pointer
+%endmacro
+
 section .data
-spin_lock: times N dq -1
-value: times N dq 0
+
+spin_lock:      times N dq -1           ; the identifier of the core that certain core is waiting for
+                                        ; i.e. core i wants to exchange values with core spin_lock[i]
+value:          times N dq 0            ; the value that certain core is offering to exchange
 
 section .text
 
-; rdi - core identifier
-; rsi - pointer to string of operations to perform
-core:
-    push r12 ; r12 will store core identifier
-    push r13 ; r13 will store pointer to string of operations to perform
-    push r14 ; r14 will store pointer to stack
-    push r15 ; r15 will store the mask to align the stack
-    mov r12, rdi
-    mov r13, rsi
-    mov r14, rsp
+core:                                   ; simulates one core of a distributed stack machine
+                                        ; rdi - core identifier
+                                        ; rsi - pointer to string of operations to perform
+                                        ; [return] - the value from the top of the stack
+                                        ; [modified] - rax, rdi, rsi, rdx, r8
 
-    ; loop through the string
+                                        ; function calls can change rdi and rsi registers so we need to
+                                        ; save their values in the registers that can't be changed
+        push    r12                     ; r12 will store core identifier
+        push    r13                     ; r13 will store pointer to string of operations to perform
+        push    r14                     ; r14 will store pointer to stack
+        push    r15                     ; r15 will store the mask to align the stack
+        mov     r12, rdi
+        mov     r13, rsi
+        mov     r14, rsp
+
+                                        ; loop through the string of operations
 .loop:
-    mov al, [r13]
+        mov     al, [r13]
 .add:
-    cmp al, '+'
-    jnz .multiply
-    ; add the top two numbers on stack
-    pop r8
-    add [rsp], r8
-    jmp .end
+        cmp     al, '+'
+        jnz     .multiply
+                                        ; add the two values from the top of the stack
+        pop     r8
+        add     [rsp], r8
+        jmp     .end
 .multiply:
-    cmp al, '*'
-    jnz .negate
-    ; multiply the top two numbers on stack
-    pop rax
-    imul qword [rsp]
-    mov [rsp], rax
-    jmp .end
+        cmp     al, '*'
+        jnz     .negate
+                                        ; multiply the two values from the top of the stack
+        pop     rax
+        imul    qword [rsp]
+        mov     [rsp], rax
+        jmp     .end
 .negate:
-    cmp al, '-'
-    jnz .number
-    ; negate the top number on stack
-    neg qword [rsp]
-    jmp .end
+        cmp     al, '-'
+        jnz     .number
+                                        ; negate the value at the top of the stack
+        neg     qword [rsp]
+        jmp     .end
 .number:
-    cmp al, '0'
-    jb .core_identifier
-    cmp al, '9'
-    ja .core_identifier
-    ; push number
-    sub al, '0'
-    movzx r8, al
-    push r8
-    jmp .end
+        cmp     al, '0'
+        jb      .core_identifier
+        cmp     al, '9'
+        ja      .core_identifier
+                                        ; push number from the string
+        sub     al, '0'
+        movzx   r8, al
+        push    r8
+        jmp     .end
 .core_identifier:
-    cmp al, 'n'
-    jnz .move
-    ; push core identifier
-    push r12
-    jmp .end
+        cmp     al, 'n'
+        jnz     .move
+                                        ; push core identifier
+        push    r12
+        jmp     .end
 .move:
-    cmp al, 'B'
-    jnz .abandon
-    ; move the pointer by a value from the top of the stack
-    pop r8
-    cmp qword [rsp], 0
-    jz  .end
-
-    cmp r8, 0
-    jg  .positive
+        cmp     al, 'B'
+        jnz     .abandon
+                                        ; move the pointer by a value from the top of the stack
+        pop     r8
+        cmp     qword [rsp], 0
+        jz      .end
+                                        ; we need to check if we should move forward or backward
+        cmp     r8, 0
+        jg      .positive
 
 .negative:
-    neg r8
-    sub r13, r8
-    jmp .end
+        neg     r8
+        sub     r13, r8
+        jmp     .end
 .positive:
-    add r13, r8
-    jmp .end
+        add     r13, r8
+        jmp     .end
 .abandon:
-    cmp al, 'C'
-    jnz .duplicate
-    ; abandon the top value from the stack
-    pop r8
-    jmp .end
+        cmp     al, 'C'
+        jnz     .duplicate
+                                        ; abandon the value from the top of the stack
+        pop     r8
+        jmp     .end
 .duplicate:
-    cmp al, 'D'
-    jnz .swap
-    ; duplicate the top value of the stack
-    push qword [rsp]
-    jmp .end
+        cmp     al, 'D'
+        jnz     .swap
+                                        ; duplicate the value from the top of the stack
+        push    qword [rsp]
+        jmp     .end
 .swap:
-    cmp al, 'E'
-    jnz .call_get
-    ; swap the top two values on stack
-    pop r8
-    xchg [rsp], r8
-    push r8
-    jmp .end
+        cmp     al, 'E'
+        jnz     .call_get
+                                        ; swap two values from the top of the stack
+        pop     r8
+        xchg    [rsp], r8
+        push    r8
+        jmp     .end
 .call_get:
-    cmp al, 'G'
-    jnz .call_put
-    ; call get_value
-    call_func_with_stack_alignment get_value
-    push rax
-    jmp .end
+        cmp     al, 'G'
+        jnz     .call_put
+                                        ; call get_value function
+        CALL_WITH_STACK_ALIGN get_value ; call function with aligned stack
+        push    rax                     ; push the value returned by get_value
+        jmp     .end
 .call_put:
-    cmp al, 'P'
-    jnz .synchronize
-    ; call put_value
-    pop rsi
-    call_func_with_stack_alignment put_value
-    jmp .end
+        cmp     al, 'P'
+        jnz     .synchronize
+                                        ; call put_value function
+        pop     rsi                     ; get the value to put
+        CALL_WITH_STACK_ALIGN put_value ; call function with aligned stack
+        jmp     .end
 .synchronize:
-    cmp al, 'S'
-    jnz .end
-    ; synchronize two cores and swap their top values
-    pop r8                      ; get m from the stack
-    mov rax, [rsp]              ; get the value to offer
-    lea rsi, [rel value]        ; get the address of value
-    lea rdi, [rel spin_lock]    ; get the address of spin_lock
-    mov [rsi + 8 * r12], rax    ; set the value to offer
-    mov [rdi + 8 * r12], r8     ; set the lock to acquire
-    lea rdx, [rdi + 8 * r8]     ; get the address of spin_lock[m]
-.busy_wait:
-    mov rax, r12                ; open lock we want to acquire
-    lock cmpxchg [rdx], rax
-    jnz .busy_wait
-    ; we acquired the lock
-    mov rax, [rsi + 8 * r8]     ; get the value to swap
-    mov [rsp], rax              ; swap the values
-    mov qword [rdx], -1         ; release the lock
+        cmp     al, 'S'
+        jnz     .end
+                                        ; synchronize two cores and then swap
+                                        ; their values from the top
+        pop     r8                      ; get value 'm' from the stack
+        mov     rax, [rsp]              ; get the value to swap with core 'm'
 
+        lea     rsi, [rel value]        ; get the address of values to exchange
+        mov     [rsi + 8 * r12], rax    ; set the value[n] to the value to exchange with core 'm',
+                                        ; no need to atomically set the value
+
+        mov     rax, r8                 ; copy the value 'm'
+        lea     rdi, [rel spin_lock]    ; get the address of spin locks
+        xchg    [rdi + 8 * r12], rax    ; set the value of spin_lock[n] to 'm' meaning that core 'n'
+                                        ; is waiting for core 'm' to exchange values
+                                        ; we used xchg to atomically set the value
+                                        ; additionally, we get the value of spin_lock[n] in rax (that is -1)
+
+        lea     rdx, [rdi + 8 * r8]     ; get the address of spin_lock[m]
+.busy_wait:
+        cmp     qword [rdx], r12        ; check if core 'm' is waiting for core 'n'
+        jnz     .busy_wait
+                                        ; we acquired the lock
+        mov     rcx, [rsi + 8 * r8]     ; get the value to swap
+        mov     [rsp], rcx              ; swap the values
+        xchg    [rdx], rax              ; release the lock, set the value of spin_lock[m] to -1
+
+        lea     rdx, [rdi + 8 * r12]    ; get the address of spin_lock[n]
 .wait:
-    cmp qword [rdi + 8 * r12], -1
-    jnz .wait
+        cmp     qword [rdx], -1         ; check if the core 'm' has already taken our value
+        jnz     .wait
 
 .end:
-    ; increment pointer
-    inc r13
-    ; check if we reached the end of the string
-    cmp byte [r13], 0
-    jnz .loop
+        inc     r13                     ; move to the next operation
+        cmp     byte [r13], 0           ; check if we reached the end of the string
+        jnz     .loop
 
 .return:
-    ; return the top value on stack
-    pop rax
-    mov rsp, r14
-    ; restore registers
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    ret
+        pop     rax                     ; return the value from the top of the stack
+        mov     rsp, r14
+                                        ; restore registers
+        pop     r15
+        pop     r14
+        pop     r13
+        pop     r12
+        ret
