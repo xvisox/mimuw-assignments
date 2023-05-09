e426ef7f0336fb429bba97e1e3b6e3e65fa300d5
xvisox <hubertmirek@gmail.com>
2023-05-09 23:34:36 +0200

    balance fix v2

--- src/minix/servers/pm/main.c
+++ src/minix/servers/pm/main.c
@@ -147,6 +147,7 @@ static int sef_cb_init_fresh(int UNUSED(type), sef_init_info_t *UNUSED(info))
   for (rmp=&mproc[0]; rmp<&mproc[NR_PROCS]; rmp++) {
 	init_timer(&rmp->mp_timer);
 	rmp->mp_magic = MP_MAGIC;
+    rmp->account_balance = INIT_BALANCE; // hm438596.init_balance
   }
 
   /* Build the set of signals which cause core dumps, and the set of signals
